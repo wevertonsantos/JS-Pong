98ab83df620dee7e6b8c818c1713a959274b429c
Weverton Santos <wever-santos@hotmail.com>
2022-10-17 08:32:47 -0300

    Função de movimenta bolinha

--- sketch.js
+++ sketch.js
@@ -3,6 +3,10 @@ let  xBolinha = 300
 let  yBolinha = 200
 let  diametro = 22
 
+// Velocidade da bolinha
+let velocidadeXBolinha = 6
+let velocidadeYBolinha = 6
+
 
 function setup() {
     createCanvas(600, 400);
@@ -11,8 +15,14 @@ function setup() {
 function draw(){
     background(0)
     mostraBolinha()
+    movimentaBolinha()
 }
 
 function mostraBolinha(){
     circle(xBolinha,yBolinha,diametro)
+  }
+
+function movimentaBolinha(){
+    xBolinha += velocidadeXBolinha
+    yBolinha += velocidadeYBolinha
   }
