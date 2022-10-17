9e0b1a63d4dbebae6af2ec1736a46b5ee845ae77
Weverton Santos <wever-santos@hotmail.com>
2022-10-17 08:35:32 -0300

    Movimentando a bolinha

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@
 let  xBolinha = 300
 let  yBolinha = 200
 let  diametro = 22
+let  raio     = diametro / 2
 
 // Velocidade da bolinha
 let velocidadeXBolinha = 6
@@ -16,6 +17,7 @@ function draw(){
     background(0)
     mostraBolinha()
     movimentaBolinha()
+    verificaColisaoBorda()
 }
 
 function mostraBolinha(){
@@ -26,3 +28,15 @@ function movimentaBolinha(){
     xBolinha += velocidadeXBolinha
     yBolinha += velocidadeYBolinha
   }
+
+  function verificaColisaoBorda(){
+    if (xBolinha + raio > width || xBolinha - raio < 0){
+    velocidadeXBolinha *= -1
+  }
+  
+  
+  if (yBolinha + raio > height || yBolinha - raio < 0){
+    velocidadeYBolinha *= -1    
+  }
+  
+}
